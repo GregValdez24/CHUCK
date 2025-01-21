d41fe877934eb3530525d10d8ac9120112dff526
Gregory Valdez <valdezga@uci.edu>
2025-01-21 11:22:57 -0800

    Initial Commit

---
+++ CHUCKTests/CHUCKTests.swift
@@ -0,0 +1,17 @@
+//
+//  CHUCKTests.swift
+//  CHUCKTests
+//
+//  Created by Greg Valdez on 1/21/25.
+//
+
+import Testing
+@testable import CHUCK
+
+struct CHUCKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
